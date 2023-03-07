89cab1d113f1febe420303fd8262ca2e3ec3321c
dasfef <96065385+dasfef@users.noreply.github.com>
2023-03-07 23:22:59 +0900

    Update DB_practice3.cpp

--- DB_practice3.cpp
+++ DB_practice3.cpp
@@ -7,9 +7,18 @@ struct students {
 	int sub3;
 };
 
+struct result {
+	int sum;
+	int avg;
+	int rank;
+};
+
 int main(void) {
 	int choice, sub1, sub2, sub3, i, j;
-	struct students students[3];
+	int cnt = 0;
+	struct students s[3];
+	struct result score[3];
+
 	while (1) {
 		printf("\t[[ 성적 처리 시스템 ]]\n");
 		printf("1. 점수 입력\n");
@@ -23,12 +32,52 @@ int main(void) {
 		case 1:
 			for (i = 0; i < 3; i++) {
 				printf("- %d번 학생\n", i + 1);
-				for (j = 0; j < 3; j++) {
-					printf("과목%d 점수 :", j + 1);
-					scanf("%d", &students[i].sub[i]);
-				}
+				printf("과목1 점수 : ");
+				scanf("%d", &s[i].sub1);
+				printf("과목2 점수 : ");
+				scanf("%d", &s[i].sub2);
+				printf("과목3 점수 : ");
+				scanf("%d", &s[i].sub3);
+
 			}
+			break;
+
+		case 2:
+			for (i = 0; i < 3; i++) {
+				score[i].sum = s[i].sub1 + s[i].sub2 + s[i].sub3;
+				score[i].avg = score[i].sum / 3;
+				
+			}
+			for (i = 0; i < 3; i++) {
+				cnt = 0;
+				for (j = 0; j < 3; j++) {
+					if (score[i].avg < score[j].avg)
+						cnt++;
+				}
+				score[i].rank = cnt + 1;
+			}
+			break;
+		
+		case 3:
+			printf("===================================================================\n");
+			printf("\t번호\t과목1\t과목2\t과목3\t총합\t평균\t순위\n");
+			printf("-------------------------------------------------------------------\n");
+			for (i = 0; i < 3; i++) {
+				printf("\t%d\t%d\t%d\t%d\t%d\t%d\t%d\n", i+1, s[i].sub1, s[i].sub2, s[i].sub3, score[i].sum, score[i].avg, score[i].rank);
+			}
+			printf("-------------------------------------------------------------------\n");
+			break;
+
+		case 4:
+			printf("프로그램 종료");
+			return 0;
+			break;
+			
+		default :
+			printf("올바른 번호를 선택해주세요\n");
+			break;
 		}
 		
+
 	}
 }
